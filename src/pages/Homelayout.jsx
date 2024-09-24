2b36a0a22db847c3a5919518bd9a715f0a95ced6
Durgesh <dkumbhare619@gmail.com>
2024-09-24 13:13:55 +0530

    Error Fixed

--- src/pages/Homelayout.jsx
+++ src/pages/Homelayout.jsx
@@ -1,10 +1,7 @@
-import React from "react";
 import Navbar from "../components/Navbar";
 import Landing from "../components/Landing";
-import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
-import Submenu from "../components/Submenu";
-import Managment from "./Managment";
+
 
 const Homelayout = () => {
   return (
